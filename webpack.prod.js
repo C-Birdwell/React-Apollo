10a925934bb3770cdda4fdd6079c786fa579854c
Colin Birdwell <Colin.birdwell@paymentdata.com>
2020-06-03 13:33:00 -0500

    Heroku production test

---
+++ webpack.prod.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = env => {
+  const isProduction = env === 'production'
+
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' })
+  return {
+    entry: './src/app.js',
+    mode: 'production',
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader'],
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      CSSExtract,
+      new webpack.DefinePlugin({
+        'process.env': {
+          CLIENT_URL: JSON.stringify(process.env.CLIENT_URL),
+        },
+      }),
+    ],
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/',
+    },
+  }
+}
